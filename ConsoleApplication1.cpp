f2b1727b085777671e6e9406683410c8bc236ddb
Ефим Бульен <143902970+EfimBulien@users.noreply.github.com>
2024-02-17 09:35:55 +0300

    first commit

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "ru");
+
+    double matrix[3][5];
+    double totalSum = 0;
+    double sum = 0;
+
+    cout << "Введите элементы матрицы 3x5\n" << endl;
+
+    for (int i = 0; i < 3; i++) {
+        cout << "Строка " << i + 1 << "\n";
+        for (int j = 0; j < 5; j++) {
+            cout << "Элемент " << j + 1 << ": ";
+            cin >> matrix[i][j];
+        }
+    }
+
+    cout << endl;
+
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 5; j++) {
+            sum += matrix[i][j];
+            totalSum += matrix[i][j];
+        }
+        cout <<  "Средние арифметическое строки " << i + 1 << ": " << sum / 5 << endl;
+        sum = 0;
+    }
+    
+    cout << "\nСреднее арифметическое всех строк: " << totalSum / 15 << endl;
+    return 0;
+}
